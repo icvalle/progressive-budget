165056b4c45f36b6256bf611f3d704a806a677e3
Iris Valle <iris.valle.design@gmail.com>
2021-07-23 18:16:24 -0400

    updated service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,9 +3,11 @@ const FILES_TO_CACHE = [
     "index.html",
     "styles.css",
     "index.js",
-    "manifest.webmanifest",
+    "dist/bundle.js",
+    "dist/manifest.json",
     "icons/icon-192x192.png",
-    "icons/icon-512x512.png"
+    "icons/icon-512x512.png",
+    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
 ];
 
 const CACHE_NAME = "static-cache-v2";
